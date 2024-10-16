9f3355f02002307f25ba54fe1129871eb5ddb84a
inkrodr22 <inaki.rodriguez.14@gmail.com>
2024-10-15 22:27:15 -0600

    Vercel configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api-gateway-jy5onp2pp-inkrodr22s-projects.vercel.app/api',
     timeout: 1000,
 });
 
